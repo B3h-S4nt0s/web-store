48bd430e653049d5056ed25553fc01dfc5ee8e02
Breno S Santos <breno0s1s6@gmail.com>
2024-10-01 14:01:13 -0400

    commit inicial

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const font = Montserrat({
+  subsets: ["latin"],
+})
+
+export const metadata: Metadata = {
+  title: "Gamer store App",
+  description: "Versão completa loja gamer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={font.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
