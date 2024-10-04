7015a48060291ab7325a6509a78c684541452c64
Breno S Santos <breno0s1s6@gmail.com>
2024-10-04 12:59:06 -0400

    changes in rules

--- src/app/produto/[id]/page.tsx
+++ src/app/produto/[id]/page.tsx
@@ -1,11 +1,11 @@
 import { produtos } from "@/core"
 
-export default function PaginaProduto(props: any):any {
+export default function PaginaProduto(props: any) {
     const id = +props.params.id // Convertion to number
-    const produto = produtos.find((produto) => produto.id === id)
+    const produto :any = produtos.find((produto) => produto.id === id)
     return (
         <div>
-            <h1>Produto: {produto?.nome}</h1>
+            <h1>Produto: {produto.nome}</h1>
         </div>
     )
 }
